074eca66e8024a8e8e15af1923a1d1608e98ceb2
Rafaela <rafaelaolbo@gmail.com>
2026-09-02 11:29:12 -0300

    adicionando arquivos

---
+++ Aula 4/AppMob/MauiAppAula4/MainPage.xaml.cs
@@ -0,0 +1,139 @@
+﻿using MauiAppAula4.Mod;
+
+namespace MauiAppAula4
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        // Soma
+        private void btnSomar_Clicked(object? sender, EventArgs e)
+        {
+            if (ValidarForm())
+            {
+                DoisParametro dp = getFormSoma();
+                setForm(dp);
+                ClearForm();
+            }
+        }
+
+        // Subtração
+        private void btnSubtrair_Clicked(object? sender, EventArgs e)
+        {
+            if (ValidarForm())
+            {
+                Subtracao sub = getFormSubtracao();
+                setForm(sub);
+                ClearForm();
+            }
+        }
+
+        // Multiplicação
+        private void btnMultiplicar_Clicked(object? sender, EventArgs e)
+        {
+            if (ValidarForm())
+            {
+                Multiplicacao mult = getFormMultiplicacao();
+                setForm(mult);
+                ClearForm();
+            }
+        }
+
+        // Divisão
+        private void btnDividir_Clicked(object? sender, EventArgs e)
+        {
+            if (ValidarForm())
+            {
+                Divisao div = getFormDivisao();
+                setForm(div);
+                ClearForm();
+            }
+        }
+
+        private void ClearForm()
+        {
+            txtNum1.Text = string.Empty;
+            txtNum2.Text = string.Empty;
+            txtNum1.Focus();
+        }
+
+        private DoisParametro getFormSoma()
+        {
+            DoisParametro dp = new DoisParametro();
+            dp.gsN1 = Convert.ToInt32(txtNum1.Text);
+            dp.gsN2 = Convert.ToInt32(txtNum2.Text);
+            return dp;
+        }
+
+        private Subtracao getFormSubtracao()
+        {
+            Subtracao sub = new Subtracao();
+            sub.gsN1 = Convert.ToInt32(txtNum1.Text);
+            sub.gsN2 = Convert.ToInt32(txtNum2.Text);
+            return sub;
+        }
+
+        private Multiplicacao getFormMultiplicacao()
+        {
+            Multiplicacao mult = new Multiplicacao();
+            mult.gsN1 = Convert.ToInt32(txtNum1.Text);
+            mult.gsN2 = Convert.ToInt32(txtNum2.Text);
+            return mult;
+        }
+
+        private Divisao getFormDivisao()
+        {
+            Divisao div = new Divisao();
+            div.gsN1 = Convert.ToInt32(txtNum1.Text);
+            div.gsN2 = Convert.ToInt32(txtNum2.Text);
+            return div;
+        }
+
+        private void setForm(DoisParametro dp)
+        {
+            int resultado = dp.Somar();
+            lblRes.Text = $"A soma de {dp.gsN1} + {dp.gsN2} = {resultado}";
+        }
+
+        private void setForm(Subtracao dp)
+        {
+            int resultado = dp.Subtrair();
+            lblRes.Text = $"A subtração de {dp.gsN1} - {dp.gsN2} = {resultado}";
+        }
+
+        private void setForm(Multiplicacao dp)
+        {
+            int resultado = dp.Multiplicar();
+            lblRes.Text = $"A multiplicação de {dp.gsN1} * {dp.gsN2} = {resultado}";
+        }
+
+        private void setForm(Divisao dp)
+        {
+            int resultado = dp.Dividir();
+            lblRes.Text = $"A divisão de {dp.gsN1} / {dp.gsN2} = {resultado}";
+        }
+
+        private bool ValidarForm()
+        {
+            if (string.IsNullOrWhiteSpace(txtNum1.Text) || !int.TryParse(txtNum1.Text, out _))
+            {
+                lblRes.Text = "Digite um número válido no Campo 1";
+                txtNum1.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtNum2.Text) || !int.TryParse(txtNum2.Text, out _))
+            {
+                lblRes.Text = "Digite um número válido no Campo 2";
+                txtNum2.Focus();
+                return false;
+            }
+
+            lblRes.Text = string.Empty;
+            return true;
+        }
+    }
+}
---
+++ Aula 4/AppMob/MauiAppAula4/Mod/DoisParametro.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MauiAppAula4.Mod
+{
+    public class DoisParametro
+    {
+        // Atributos
+        int aN1 = 0;
+        int aN2 = 0;
+        int aS = 0;
+
+        // Método construtor
+        public DoisParametro() { }
+
+        // Métodos de acesso
+        public int gsN1 { get => aN1; set => aN1 = value; }
+        public int gsN2 { get => aN2; set => aN2 = value; }
+        public int gsS { get => aS; set => aS = value; }
+
+        // Métodos específicos
+        public int Somar()
+        {
+            gsS = gsN1 + gsN2;
+            return gsS;
+        }
+
+    }
+
+    public class Subtracao 
+    {
+        // Atributos
+        int aN1 = 0;
+        int aN2 = 0;
+        int aS = 0;
+
+        // Método construtor
+        public Subtracao() { }
+
+        // Métodos de acesso
+        public int gsN1 { get => aN1; set => aN1 = value; }
+        public int gsN2 { get => aN2; set => aN2 = value; }
+        public int gsS { get => aS; set => aS = value; }
+
+        // Métodos específicos
+        public int Subtrair()
+        {
+            gsS = gsN1 - gsN2;
+            return gsS;
+        }
+    }
+
+    public class Multiplicacao
+    {
+        // Atributos
+        int aN1 = 0;
+        int aN2 = 0;
+        int aS = 0;
+
+        // Método construtor
+        public Multiplicacao() { }
+
+        // Métodos de acesso
+        public int gsN1 { get => aN1; set => aN1 = value; }
+        public int gsN2 { get => aN2; set => aN2 = value; }
+        public int gsS { get => aS; set => aS = value; }
+
+        // Métodos específicos
+        public int Multiplicar()
+        {
+            gsS = gsN1 * gsN2;
+            return gsS;
+        }
+    }
+
+    public class Divisao
+    {
+        // Atributos
+        int aN1 = 0;
+        int aN2 = 0;
+        int aS = 0;
+
+        // Método construtor
+        public Divisao() { }
+
+        // Métodos de acesso
+        public int gsN1 { get => aN1; set => aN1 = value; }
+        public int gsN2 { get => aN2; set => aN2 = value; }
+        public int gsS { get => aS; set => aS = value; }
+
+        // Métodos específicos
+        public int Dividir()
+        {
+            gsS = gsN1 / gsN2;
+            return gsS;
+        }
+    }
+}
